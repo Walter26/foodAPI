a26b54ac8015a09214a9accab8f1c0f668e0c81e
walter26 <walterrafael26@gmail.com>
2020-07-02 23:35:51 -0600

    Fix typo

--- models/RecipeModel.js
+++ models/RecipeModel.js
@@ -20,7 +20,7 @@ var IngredientSchema = mongoose.Schema({
 }, {timestamps: false})
 
 var RecipeSchema = Schema({
-    autor : {
+    author : {
         type: String,
         required: true
     },
