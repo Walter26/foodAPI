221d6f7a46f25f4185c80b66c5ffa1c62f7d042a
walter26 <walterrafael26@gmail.com>
2020-07-09 01:54:00 -0600

    Fix mongoose bug

--- models/RecipeModel.js
+++ models/RecipeModel.js
@@ -30,8 +30,8 @@ var RecipeSchema = Schema({
     },
     desc: String,
     recipeType: [String],
-    steps: [StepSchema],
-    ingredients: [IngredientSchema],
+    steps: Schema.Types.Mixed,
+    ingredients: Schema.Types.Mixed,
     privacy: {
         type: Boolean,
         required: true
